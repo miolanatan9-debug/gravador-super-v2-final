58b303f1e03554ccd4d0fe454db89e3ef954d190
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 17:32:53 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/activities/MainActivity.kt from repo_final

---
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/activities/MainActivity.kt
@@ -0,0 +1,301 @@
+package br.com.guga.gravadorsuper.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import me.grantland.widget.AutofitHelper
+import org.fossify.commons.extensions.appLaunched
+import org.fossify.commons.extensions.checkAppSideloading
+import org.fossify.commons.extensions.getAlertDialogBuilder
+import org.fossify.commons.extensions.getBottomNavigationBackgroundColor
+import org.fossify.commons.extensions.hideKeyboard
+import org.fossify.commons.extensions.onPageChangeListener
+import org.fossify.commons.extensions.onTabSelectionChanged
+import org.fossify.commons.extensions.toast
+import org.fossify.commons.extensions.updateBottomTabItemColors
+import org.fossify.commons.helpers.PERMISSION_RECORD_AUDIO
+import org.fossify.commons.helpers.PERMISSION_WRITE_STORAGE
+import org.fossify.commons.helpers.isRPlus
+import br.com.guga.gravadorsuper.R
+import br.com.guga.gravadorsuper.adapters.ViewPagerAdapter
+import br.com.guga.gravadorsuper.databinding.ActivityMainBinding
+import br.com.guga.gravadorsuper.extensions.config
+import br.com.guga.gravadorsuper.extensions.deleteExpiredTrashedRecordings
+import br.com.guga.gravadorsuper.extensions.ensureStoragePermission
+import br.com.guga.gravadorsuper.helpers.STOP_AMPLITUDE_UPDATE
+import br.com.guga.gravadorsuper.models.Events
+import br.com.guga.gravadorsuper.services.RecorderService
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+
+class MainActivity : SimpleActivity() {
+
+    private var bus: EventBus? = null
+
+    override var isSearchBarEnabled = true
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        appLaunched(BuildConfig.APPLICATION_ID)
+        setupOptionsMenu()
+        refreshMenuItems()
+
+        setupEdgeToEdge(padBottomImeAndSystem = listOf(binding.mainTabsHolder))
+
+        if (checkAppSideloading()) {
+            return
+        }
+
+        if (savedInstanceState == null) {
+            deleteExpiredTrashedRecordings()
+        }
+
+        handlePermission(PERMISSION_RECORD_AUDIO) {
+            if (it) {
+                tryInitVoiceRecorder()
+            } else {
+                toast(org.fossify.commons.R.string.no_audio_permissions)
+                finish()
+            }
+        }
+
+        bus = EventBus.getDefault()
+        bus!!.register(this)
+        if (config.recordAfterLaunch && !RecorderService.isRunning) {
+            Intent(this@MainActivity, RecorderService::class.java).apply {
+                try {
+                    startService(this)
+                } catch (ignored: Exception) {
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateMenuColors()
+        if (getPagerAdapter()?.showRecycleBin != config.useRecycleBin) {
+            setupViewPager()
+        }
+        setupTabColors()
+        getPagerAdapter()?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        config.lastUsedViewPagerPage = binding.viewPager.currentItem
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bus?.unregister(this)
+        getPagerAdapter()?.onDestroy()
+
+        Intent(this@MainActivity, RecorderService::class.java).apply {
+            action = STOP_AMPLITUDE_UPDATE
+            try {
+                startService(this)
+            } catch (ignored: Exception) {
+            }
+        }
+    }
+
+    override fun onBackPressedCompat(): Boolean {
+        return if (binding.mainMenu.isSearchOpen) {
+            binding.mainMenu.closeSearch()
+            true
+        } else if (isThirdPartyIntent()) {
+            setResult(Activity.RESULT_CANCELED, null)
+            false
+        } else {
+            false
+        }
+    }
+
+    private fun refreshMenuItems() {
+        binding.mainMenu.requireToolbar().menu.apply {
+            findItem(R.id.delete_all).isVisible = binding.viewPager.currentItem != 0
+        }
+    }
+
+    private fun setupOptionsMenu() {
+        binding.mainMenu.requireToolbar().inflateMenu(R.menu.menu)
+        binding.mainMenu.toggleHideOnScroll(false)
+        binding.mainMenu.setupMenu()
+
+        binding.mainMenu.onSearchOpenListener = {
+            if (binding.viewPager.currentItem == 0) {
+                binding.viewPager.currentItem = 1
+            }
+        }
+
+        binding.mainMenu.onSearchTextChangedListener = { text ->
+            getPagerAdapter()?.searchTextChanged(text)
+        }
+
+        binding.mainMenu.requireToolbar().setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.delete_all -> getPagerAdapter()?.deleteAll(binding.viewPager.currentItem)
+                R.id.settings -> launchSettings()
+                R.id.how_to -> launchHowTo()
+                R.id.about -> launchAbout()
+                else -> return@setOnMenuItemClickListener false
+            }
+            return@setOnMenuItemClickListener true
+        }
+    }
+
+    private fun updateMenuColors() {
+        binding.mainMenu.updateColors()
+    }
+
+    private fun tryInitVoiceRecorder() {
+        if (isRPlus()) {
+            ensureStoragePermission { granted ->
+                if (granted) {
+                    setupViewPager()
+                } else {
+                    toast(org.fossify.commons.R.string.no_storage_permissions)
+                    finish()
+                }
+            }
+        } else {
+            handlePermission(PERMISSION_WRITE_STORAGE) {
+                if (it) {
+                    setupViewPager()
+                } else {
+                    toast(org.fossify.commons.R.string.no_storage_permissions)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun setupViewPager() {
+        binding.mainTabsHolder.removeAllTabs()
+        var tabDrawables = arrayOf(
+            org.fossify.commons.R.drawable.ic_microphone_vector,
+            R.drawable.ic_playlist_play_vector
+        )
+        var tabLabels = arrayOf(R.string.recorder, R.string.player)
+        if (config.useRecycleBin) {
+            tabDrawables += org.fossify.commons.R.drawable.ic_delete_vector
+            tabLabels += org.fossify.commons.R.string.recycle_bin
+        }
+
+        tabDrawables.forEachIndexed { i, drawableId ->
+            binding.mainTabsHolder.newTab()
+                .setCustomView(org.fossify.commons.R.layout.bottom_tablayout_item).apply {
+                    customView
+                        ?.findViewById<ImageView>(org.fossify.commons.R.id.tab_item_icon)
+                        ?.setImageDrawable(
+                            AppCompatResources.getDrawable(
+                                this@MainActivity,
+                                drawableId
+                            )
+                        )
+
+                    customView
+                        ?.findViewById<TextView>(org.fossify.commons.R.id.tab_item_label)
+                        ?.setText(tabLabels[i])
+
+                    AutofitHelper.create(
+                        customView?.findViewById(org.fossify.commons.R.id.tab_item_label)
+                    )
+
+                    binding.mainTabsHolder.addTab(this)
+                }
+        }
+
+        binding.mainTabsHolder.onTabSelectionChanged(
+            tabUnselectedAction = {
+                updateBottomTabItemColors(it.customView, false)
+                if (it.position == 1 || it.position == 2) {
+                    binding.mainMenu.closeSearch()
+                }
+            },
+            tabSelectedAction = {
+                binding.viewPager.currentItem = it.position
+                updateBottomTabItemColors(it.customView, true)
+                refreshMenuItems()
+            }
+        )
+
+        binding.viewPager.adapter = ViewPagerAdapter(this, config.useRecycleBin)
+        binding.viewPager.offscreenPageLimit = 2
+        binding.viewPager.onPageChangeListener {
+            binding.mainTabsHolder.getTabAt(it)?.select()
+            (binding.viewPager.adapter as ViewPagerAdapter).finishActMode()
+            refreshMenuItems()
+        }
+
+        if (isThirdPartyIntent()) {
+            binding.viewPager.currentItem = 0
+        } else {
+            binding.viewPager.currentItem = config.lastUsedViewPagerPage
+            binding.mainTabsHolder.getTabAt(config.lastUsedViewPagerPage)?.select()
+        }
+    }
+
+    private fun setupTabColors() {
+        val activeView = binding.mainTabsHolder.getTabAt(binding.viewPager.currentItem)?.customView
+        updateBottomTabItemColors(activeView, true)
+        for (i in 0 until binding.mainTabsHolder.tabCount) {
+            if (i != binding.viewPager.currentItem) {
+                val inactiveView = binding.mainTabsHolder.getTabAt(i)?.customView
+                updateBottomTabItemColors(inactiveView, false)
+            }
+        }
+
+        binding.mainTabsHolder.getTabAt(binding.viewPager.currentItem)?.select()
+        val bottomBarColor = getBottomNavigationBackgroundColor()
+        binding.mainTabsHolder.setBackgroundColor(bottomBarColor)
+    }
+
+    private fun getPagerAdapter() = (binding.viewPager.adapter as? ViewPagerAdapter)
+
+    private fun launchSettings() {
+        hideKeyboard()
+        startActivity(Intent(applicationContext, SettingsActivity::class.java))
+    }
+
+    private fun launchAbout() {
+        showInfoDialog(R.string.about, R.string.about_content)
+    }
+
+    private fun launchHowTo() {
+        showInfoDialog(R.string.how_to, R.string.how_to_content)
+    }
+
+    private fun showInfoDialog(titleRes: Int, contentRes: Int) {
+        getAlertDialogBuilder()
+            .setTitle(titleRes)
+            .setMessage(contentRes)
+            .setPositiveButton(org.fossify.commons.R.string.ok, null)
+            .show()
+    }
+
+    private fun isThirdPartyIntent() = intent?.action == MediaStore.Audio.Media.RECORD_SOUND_ACTION
+
+    @Suppress("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun recordingSaved(event: Events.RecordingSaved) {
+        if (isThirdPartyIntent()) {
+            Intent().apply {
+                data = event.uri!!
+                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                setResult(Activity.RESULT_OK, this)
+            }
+            finish()
+        }
+    }
+}
