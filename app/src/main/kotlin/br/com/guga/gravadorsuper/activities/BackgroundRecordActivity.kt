a483e687967a312c47a4dd7d8cf4b8540052820b
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 17:32:52 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/activities/BackgroundRecordActivity.kt from repo_final

---
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/activities/BackgroundRecordActivity.kt
@@ -0,0 +1,40 @@
+package br.com.guga.gravadorsuper.activities
+
+import android.content.Intent
+import org.fossify.commons.dialogs.PermissionRequiredDialog
+import org.fossify.commons.extensions.openNotificationSettings
+import br.com.guga.gravadorsuper.services.RecorderService
+
+class BackgroundRecordActivity : SimpleActivity() {
+    companion object {
+        const val RECORD_INTENT_ACTION = "RECORD_ACTION"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (intent.action == RECORD_INTENT_ACTION) {
+            handleNotificationPermission { granted ->
+                if (granted) {
+                    Intent(this@BackgroundRecordActivity, RecorderService::class.java).apply {
+                        try {
+                            if (RecorderService.isRunning) {
+                                stopService(this)
+                            } else {
+                                startService(this)
+                            }
+                        } catch (ignored: Exception) {
+                        }
+                    }
+                } else {
+                    PermissionRequiredDialog(
+                        activity = this,
+                        textId = org.fossify.commons.R.string.allow_notifications_voice_recorder,
+                        positiveActionCallback = { openNotificationSettings() }
+                    )
+                }
+            }
+        }
+        moveTaskToBack(true)
+        finish()
+    }
+}
